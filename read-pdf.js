5931256824004827eaf17835bca857c4c847c870
Sharif Mohammad Nasrullah <sharifmohammadnasrullah@Sharifs-Mac-mini.local>
2026-04-26 13:07:07 +0600

    chore: Clean up workspace - remove dev artifacts, old screenshots, and unnecessary files

--- read-pdf.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-let dataBuffer = fs.readFileSync('/Users/sharifmohammadnasrullah/Downloads/Hayat Life Care Brochure-2026-2D-3mb (1).pdf');
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-});
